1be76a6b267b076122de953a0931910f98d842f2
steevecode <stevecode@yahoo.com>
2021-08-21 13:35:38 +0100

    initial commit

---
+++ src/main/java/com/twitter/marketing/bot/util/Constants.java
@@ -0,0 +1,60 @@
+package com.twitter.marketing.bot.util;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Constants {
+
+    public static final String POST_TWEET_URL = "https://api.twitter.com/...............";
+
+    public static final String AUTHORIZATION = "Authorization";
+
+    public static final String STATUS = "status";
+
+    public static final Integer MAX_TWEET_LENGTH = 280;
+
+    public static final List<String> realEstateHashtagsList = Arrays.asList(
+            "pazinoecc",
+            "RealEstateIsKing",
+            "#property",
+            "#forsale",
+            "#forrent",
+            "#realestateagent",
+            "#newhome",
+            "#dreamhome",
+            "#househunting",
+            "#apartmenthunting",
+            "#sold",
+            "#homesforsale",
+            "#realestate",
+            "#realtor",
+            "#realestateagent",
+            "#realtorlife",
+            "#investment",
+            "#interiordesign",
+            "#architecture"
+
+    );
+
+    public static final List<String> pazinoGardensList = Arrays.asList(
+            "https://pazinoecc.com/",
+            "https://pazinoecc.com/gardenia-homes-gardens/",
+            "https://pazinoecc.com/bliss-ville-gardens/",
+            "https://pazinoecc.com/gardenia-homes-gardens/",
+            "https://pazinoecc.com/masters-villa/",
+            "https://pazinoecc.com/bliss-ville-gardens/"
+    );
+
+    public static final List<String> realEstateQuoteList = Arrays.asList(
+            "Owning a home is a keystone of wealth…both financial affluence and emotional security",
+            "Landlords grow rich in their sleep",
+            "Never give up on your dream",
+            "Ninety percent of all millionaires become so through owning real estate",
+            "The best investment on Earth is earth",
+            "The major fortunes in America have been made in land",
+            "You're so much stronger than your excuses",
+            "Success in real estate starts when you believe you are worthy of it",
+            "Hold the vision, trust the process",
+            "Some people want it to happen, some wish it would happen, others make it happen"
+    );
+}
